891ebba58cf9c07b452ca761d8a3c379f88906e7
NatalyBlack <140597696+NatalyBlack@users.noreply.github.com>
2023-11-15 14:37:05 +0100

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,43 +1,54 @@
-        print(f"Random Number: {item}")
-    return data
-    data = generate_random_data()
-if __name__ == "__main__":
-import random
+    for (let i = 0; i < 10; i++) {
+}
+function main() {
+        data.push(Math.floor(Math.random() * 100) + 1);
 
-    data = [random.randint(1, 100) for _ in range(10)]
-
-    for item in data:
-
-    main()
-def main():
-
-def generate_random_data():    const data = [];
-    }
-
-    return data;
+}
     data.forEach(item => {
+    }
+    });
+    const data = [];
     const data = generateRandomData();
 main();
-function main() {
-    });
+    return data;
 function generateRandomData() {
-}
-    for (let i = 0; i < 10; i++) {
-
-}
 
         console.log("Random Number: " + item);
-        data.push(Math.floor(Math.random() * 100) + 1);
-def generate_random_data():
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+}
+        }
+        for (int i = 0; i < 10; i++) {
+
+
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
+        int[] data = new int[10];
+        }
+    }
+        for (int item : data) {def generate_random_data():
 def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-
-if __name__ == "__main__":
-
-    for item in data:
-
-    return data
-        print(f"Random Number: {item}")
     main()
-    data = generate_random_data()
+
+        print(f"Random Number: {item}")
 import random
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+
+
+    data = generate_random_data()
+    for item in data:
+if __name__ == "__main__":}
+public class RandomDataGenerator {
+    }
+        for (int i = 0; i < 10; i++) {
+        }
+        }
+        for (int item : data) {
+
+        int[] data = new int[10];
+    public static void main(String[] args) {
+
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
